3487c513ab4a93efe5d6fc9b1ffff870206bbadf
ahmadihsan <oplikhsan@gmail.com>
2022-10-10 11:14:24 +0700

    Tugas-1 membuat kasus oop sederhana

--- oop/Animal.php
+++ oop/Animal.php
@@ -3,10 +3,6 @@
 # membuat class Animal
 class Animal
 {
-    # property animals
-
-    # method constructor - mengisi data awal
-    # parameter: data hewan (array)
 
     public array $animals = [];
     public function __construct(array $data = [])
@@ -14,7 +10,6 @@ class Animal
         $this->animals = $data;
     }
 
-    # method index - menampilkan data animals
     public function index()
     {
         foreach ($this->animals as $key => $animal) {
@@ -22,23 +17,16 @@ class Animal
         }
     }
 
-    # method store - menambahkan hewan baru
-    # parameter: hewan baru
     public function store($data)
     {
-        # gunakan method array_push untuk menambahkan data baru
         array_push($this->animals, $data);
     }
 
-    # method update - mengupdate hewan
-    # parameter: index dan hewan baru
     public function update($index, $data)
     {
         $this->animals[$index] = $data;
     }
 
-    # method delete - menghapus hewan
-    # parameter: index
     public function destroy($index)
     {
         unset($this->animals[$index]);
